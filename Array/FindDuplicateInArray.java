4039978c5e372a944f821d95975e0ab461082c41
TheDarkMystic <jaykadam9@gmail.com>
2019-02-09 15:38:28 +0530

    Added some solutions.

---
+++ Array/FindDuplicateInArray.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interviewprep.Array;
+
+/**
+ *
+ * @author jakadam
+ */
+import java.util.List;
+public class FindDuplicateInArray {
+    public int repeatedNumber(final List<Integer> a) {
+	    boolean[] arr = new boolean[a.size()];
+	    
+	    
+	    for(int i = 0 ; i < a.size() ; i ++){
+	       int tmp = a.get(i)-1;
+	       if(arr[tmp]==true){
+	           return tmp+1;
+	       }else{
+	       arr[tmp] = true ; 
+	       }
+	    }
+	    return -1;
+	    
+	}
+}
+
+
+
+/*
+ProblemURLs:
+I-https://www.interviewbit.com/problems/find-duplicate-in-array/
+L-https://leetcode.com/problems/find-the-duplicate-number/
+*/
