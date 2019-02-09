b33a89232a65bc164e443b9bbe7b47c06bbcb400
TheDarkMystic <jaykadam9@gmail.com>
2019-02-09 15:49:32 +0530

    Added some solutions.

---
+++ Array/N3RepeatNumber.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interviewprep.Array;
+
+/**
+ *
+ * @author jakadam
+ */
+import java.util.*;
+public class N3RepeatNumber {
+    public int repeatedNumber(final List<Integer> nums) {
+	    
+	    int len= nums.size(),
+        cand1=0,
+        cand2=0,
+        count1=0,
+        count2=0;
+        
+        
+        //phase1 [1,2,2,3,2,1,1,3]=>[2,1]//order of if else is ver 
+        //important
+        for(int n: nums){
+            if(n==cand1) count1++;
+                    else 
+                        if(n==cand2) count2++;
+                        else
+                            if(count1==0){
+                                cand1=n; 
+                                count1=1;
+                            }
+                            else  
+                                if(count2==0){
+                                    cand2=n;
+                                    count2=1;
+                                } 
+                                else{
+                                        count1--;
+                                        count2--;
+                                    }
+        }
+        
+        //phase2
+        count1=0; 
+        count2=0;
+        for(int n:nums){
+            if(n==cand1)
+                count1++;
+            else if(n==cand2)
+                count2++;
+        }
+        
+        //Set<Integer> set= new HashSet<>();
+        // avoid duplicates in result[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]=
+        //>[0,0]... which is not correct
+        
+        if(count1>len/3) return cand1;
+        else
+            if(count2>len/3) return cand2;
+	    
+	    return -1;
+	}
+}
+
+
+/*
+*
+ProblemURLs:-
+I-https://www.interviewbit.com/problems/n3-repeat-number/
+
+NOTES:
+Boyer-moore algo for majority counting
+
+https://www.youtube.com/watch?v=zOyOwDEF1Rc
+
+n/3 variation
+https://discuss.leetcode.com/topic/17396/boyer-moore-majority-vote-algorithm-generalization/5
+
+CTRL+F :doudou900914 
+
+n/2 algorithm variation
+https://leetcode.com/problems/majority-element/#/description
+
+soltion using Sorting is in complete solution section of IB
+*/
---
+++ Array/RepeatMissingNumberArray.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interviewprep.Array;
+
+/**
+ *
+ * @author jakadam
+ */
+import java.util.*;
+public class RepeatMissingNumberArray {
+    
+    public ArrayList<Integer> repeatedNumber(final List<Integer> a) {
+	    int arraySize = a.size();
+	    long sumOfNumbers = getSumOfNumbers(arraySize);
+	    long sumOfSquares = getSumOfSquares(arraySize);
+	    long differenceNumber =  getDifferenceofNumbers(a,sumOfNumbers);
+	    long differenceSquare = getDifferenceofSquares(a,sumOfSquares);
+	    long sumNumber =  differenceSquare/differenceNumber;
+	    int repeatedNumber = (int)((sumNumber+differenceNumber)/2);
+	    int missingNumber = (int)(sumNumber-repeatedNumber);
+	    ArrayList<Integer> result = new ArrayList<>();
+	    result.add(repeatedNumber);
+	    result.add(missingNumber);
+	    return result;
+	}
+	
+	private long getDifferenceofNumbers(List<Integer> a,long sumOfNumbers){
+	    long sum = sumOfNumbers*-1;
+	    for(Integer number:a){
+	        long num = (long)number;
+	        sum+=num;;
+	    }
+	    return sum;
+	}
+	
+	private long getDifferenceofSquares(List<Integer> a, long sumOfSquares){
+	    long sumSquares = sumOfSquares*-1;
+	    for(Integer number:a){
+	         long num = (long)number;
+	        sumSquares+=(num*num);
+	    }
+	    return sumSquares;
+	}
+	
+	private long getSumOfNumbers(double n){
+	    return (long)(n*(n-1)/2+n);
+	}
+	
+	private long getSumOfSquares(double n){
+	    return (long)(n*(n+1)*(2*n+1)/6);
+	}
+    
+}
+/*
+ProblemURLs:
+I-https://www.interviewbit.com/problems/repeat-and-missing-number-array/
+
+NOTES:
+problem in the judge
+correct solution but complexity not accepted.
+
+It is O(N). Many people complained in comment.
+
+
+https://leisurehours.wordpress.com/2009/08/13/one-duplicate-one-missing-element-in-an-array/
+
+
+http://www.geeksforgeeks.org/?p=11946/comment-page-1#comment-3796
+
+Working solution in Python( if Java Solution Fails due to casting issues)
+
+class Solution:
+    # @param A : tuple of integers
+    # @return a list of integers
+    def repeatedNumber(self, A):
+        sumOfA = 0
+        sumOfA2 = 0
+        n = 0
+        for a in A:
+            sumOfA2 += a*a
+            sumOfA += a
+            n += 1
+        sumOfN = n*(n+1)/2
+        retA = sumOfN - sumOfA
+        
+        retB = (sumOfN*(2*n+1)/3 - sumOfA2)/retA
+        x = (retB-retA)/2
+        return [x,x + retA]
+
+
+
+
+*/
