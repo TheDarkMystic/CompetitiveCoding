b72d443a16731349f6d13f1e9e24dbe2a474e117
TheDarkMystic <jaykadam9@gmail.com>
2019-02-10 17:45:58 +0530

    Adding Alt Solution.

--- Array/SimulationArray/KthRowOfPascalTriangle.java
+++ Array/SimulationArray/KthRowOfPascalTriangle.java
@@ -11,20 +11,32 @@ package interviewprep.Array.SimulationArray;
  */
 import java.util.*;
 public class KthRowOfPascalTriangle {
-    public ArrayList<Integer> getRow(int rowNum) {
-	    int A[] = new int[rowNum+1];
-        Arrays.fill(A,0);
-        A[0]=1;
+//    public ArrayList<Integer> getRow(int rowNum) {
+//	    int A[] = new int[rowNum+1];
+//        Arrays.fill(A,0);
+//        A[0]=1;
+//        
+//        for(int i=1;i<=rowNum; i++)
+//            for(int j=i;j>0;j--)
+//                A[j]=A[j]+A[j-1];
+//	    
+//	    ArrayList<Integer> sol= new ArrayList<Integer>();
+//	    for(int ele: A)
+//	        sol.add(ele);
+//	    return sol;
+//	}
+     public ArrayList<Integer> getRow(int A) {
+        int lineNum=A+1;
+        int prevEle=1;
+        ArrayList<Integer> res = new ArrayList<Integer>();
         
-        for(int i=1;i<=rowNum; i++)
-            for(int j=i;j>0;j--)
-                A[j]=A[j]+A[j-1];
-	    
-	    ArrayList<Integer> sol= new ArrayList<Integer>();
-	    for(int ele: A)
-	        sol.add(ele);
-	    return sol;
-	}
+        for(int eleNum=1; eleNum<=lineNum; eleNum++){
+            res.add(prevEle);
+            prevEle=prevEle*(lineNum-eleNum)/eleNum;
+        }
+        
+        return res;
+    }
 }
 
 
@@ -32,8 +44,16 @@ public class KthRowOfPascalTriangle {
 ProblemUrls:
 I-https://www.interviewbit.com/problems/kth-row-of-pascals-triangle/
 NOTES:
+For Commented Solution
 https://discuss.leetcode.com/topic/2510/here-is-my-brief-o-k-solution/5
 
-Dry run and try to 
-imagine
+For Uncommented Solution:
+https://en.wikipedia.org/wiki/Pascal%27s_triangle#Calculating_a_row_or_diagonal_by_itself
+The Kth Row of pascal triangle can be calculated directly using the formula
+
+initialize first ele as prev=1
+and calculate subsequent eles using 
+
+pre=pre*(lineNo. - eleNo.)/(eleNo)
+
 */
