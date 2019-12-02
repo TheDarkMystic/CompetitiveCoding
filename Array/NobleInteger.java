abb02650112eb11c7dd1e0966ec0cff2fb201aa4
TheDarkMystic <er.jayk@gmail.com>
2019-12-02 22:37:49 +0530

    optimised code for Array/NobleInteger.java

--- Array/NobleInteger.java
+++ Array/NobleInteger.java
@@ -8,32 +8,40 @@ package interviewprep.Array.Bucketing;
  *
  * @author jakadam
  */
-
 import java.util.*;
 
 public class NobleInteger {
-        public int solve(ArrayList<Integer> A) {
-        int len=A.size();
-        int ans=-1;
+
+    public int solve(ArrayList<Integer> A) {
+        /*
+        1. Sort the Array arr[] in ascending order. This step takes (O(nlogn)).
+        2. Iterate through the array. Compare the value of index i to the number of elements 
+        after index i. If arr[i] equals the number of elements after arr[i], it is a noble 
+        Integer. Condition to check: (A[i] == length-i-1). This step takes O(n).
+        Note: Array may have duplicate elements. So, we should skip the elements 
+        (adjacent elements in the sorted array) that are same.
+         */
+        int len = A.size();
+        int ans = -1;
         Collections.sort(A);
-        
-        
-        for(int i=0; i<len;i++){
-            
-            //if A[i]==A[i+1], skip to next ele. 
-            //First condition avoids AIndexOutOfBounds for testing last ele
-            if(i<len-1 && A.get(i)==A.get(i+1))
+
+        // Return a Noble element if present 
+        // before last. 
+        for (int i = 0; i < len - 1; i++) {
+            if (A.get(i) == A.get(i + 1)) {
                 continue;
-            //count the number of eles > A[i]
-            if(A.get(i)==len-1-i ){
-                ans=1;
-                //System.out.println(A.get(i));
-                break;
+            }
+            // In case of duplicates, we 
+            // reach last occurrence here. 
+            if (A.get(i) == len - i - 1) {
+                return 1;
             }
         }
-        
-        return ans;
-        
+
+        if (A.get(len - 1) == 0) {
+            return 1;
+        }
+        return -1;
     }
 }
 
@@ -43,4 +51,4 @@ I-https://www.interviewbit.com/problems/noble-integer/
 
 NOTES:
 http://www.geeksforgeeks.org/noble-integers-in-an-array-count-of-greater-elements-is-equal-to-value/
-*/
+ */
